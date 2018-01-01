f1b7b967557ecbec74ba817fee7e1255416f6026
Frances Coronel <hello@fvcproductions.com>
2018-01-01 12:20:01 -0500

    Automatic deployments
    Signed-off-by: Frances Coronel <hello@fvcproductions.com>

--- src/js/components/serviceWorker.js
+++ src/js/components/serviceWorker.js
@@ -2,10 +2,10 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/sw.js")
-      .then((registration) => {
+      .then(registration => {
         console.log("SW registered: ", registration);
       })
-      .catch((registrationError) => {
+      .catch(registrationError => {
         console.log("SW registration failed: ", registrationError);
       });
   });
